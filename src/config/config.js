16556fd2ca44bef6540a4126c18e5ea66dee80ea
leoakira345 <jinmishima996@gmail.com>
2025-11-24 10:24:32 +0530

    Add files via upload

---
+++ src/config/config.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+
+module.exports = {
+    PORT: process.env.PORT || 3000,
+    NODE_ENV: process.env.NODE_ENV || 'development',
+    STUN_SERVERS: [
+        'stun:stun.l.google.com:19302',
+        'stun:stun1.l.google.com:19302'
+    ]
+};
